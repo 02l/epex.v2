ce5ed9be6e81016a72cd1ee380341bf57dda4568
02l <mitrakos12@gmail.com>
2017-11-29 18:31:19 -0500

    player and user profile modal setup

---
+++ src/app/components/modalprofile/modalprofile.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modalprofile',
+  templateUrl: './modalprofile.component.html',
+  styleUrls: ['./modalprofile.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ModalprofileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
